3292bc41e0a17e9ed876050497575c4fd556c530
raihan hakim <raihanhakim@192.168.1.22>
2026-05-22 06:18:59 +0700

    Remove copyright page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,7 @@
-import Link from "next/link";
-
 export default function Footer() {
   return (
     <footer className="mx-auto max-w-6xl px-4 pb-8 pt-4 text-center text-xs font-bold uppercase tracking-[0.18em] text-emerald-900/70">
-      <Link href="/copyright" className="underline-offset-4 hover:underline">&copy; 2026 KareFacts. Copyright by kareayamq.</Link>
+      <p>&copy; 2026 KareFacts. Copyright by kareayamq.</p>
     </footer>
   );
 }
