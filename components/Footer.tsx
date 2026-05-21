476f4dbecf5bba630d32416d9d233878adcda18f
raihan hakim <raihanhakim@192.168.1.22>
2026-05-22 06:14:46 +0700

    Add copyright page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,9 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <footer className="mx-auto max-w-6xl px-4 pb-8 pt-4 text-center text-xs font-bold uppercase tracking-[0.18em] text-emerald-900/70">
-      <p>&copy; 2026 KareFacts. Copyright by kareayamq.</p>
+      <Link href="/copyright" className="underline-offset-4 hover:underline">&copy; 2026 KareFacts. Copyright by kareayamq.</Link>
     </footer>
   );
 }
